62a1ead130097a7b5c0471b66db7ba443921cbd0
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-07-16 21:25:53 -0400

    Add useState, useEffect and HTTP request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/ui/Header";
 import "./App.css";
 
 const App = () => {
+  // within the [] you define the state (the items we are calling from the API) and the function that will be used to manipulate the state
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
